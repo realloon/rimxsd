06b17807bfd029ee0b3d2f25f5f2ea8a4b1a57e6
sincerajn <sincerajn@outlook.com>
2025-07-09 02:30:31 +0800

    Add a test case

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-import { test } from './test/index.test'
-
+import {test} from './test/index.test'
 test()
+
+// // Assets
+// import csCode from './assets/Type.txt'
+// // Modules
+// import { parseCSharp, buildXsdDom, serializeXsd } from './modules'
+
+// export function main() {
+//   const irArr = parseCSharp(csCode)
+//   const dom = buildXsdDom(irArr)
+//   const xsd = serializeXsd(dom)
+//   console.log(xsd)
+// }
+// main()
