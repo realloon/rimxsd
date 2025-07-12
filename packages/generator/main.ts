f2f5370d3e4d9bfef0910a5ff60fee0050675d37
sincerajn <sincerajn@outlook.com>
2025-07-13 04:55:26 +0800

    ~

--- packages/generator/main.ts
+++ packages/generator/main.ts
@@ -1,14 +1,11 @@
 // Assets
-import code from '../../assets/code.txt'
+import code from '../../code.tmpl.txt'
 // Modules
 import { parseCSharp } from './parseCSharp'
 import { buildXsdDom } from './buildXsdDom'
 import { serializeXsd } from './serializeXsd'
 
-export function main() {
-  const irArr = parseCSharp(code)
-  const dom = buildXsdDom(irArr)
-  const xsd = serializeXsd(dom)
-  console.log(xsd)
-}
-main()
+const irArr = parseCSharp(code)
+const dom = buildXsdDom(irArr)
+const xsd = serializeXsd(dom)
+console.log(xsd)
